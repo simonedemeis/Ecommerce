0d21d6c63b8d3496a8f463749e5d0eb4c04d3d92
simonedemeis <simonedemeis97@gmail.com>
2025-02-18 15:21:03 +0100

    Added display attribute

--- Ecommerce/Models/AddProduct.cs
+++ Ecommerce/Models/AddProduct.cs
@@ -4,21 +4,23 @@ namespace Ecommerce.Models;
 
 public class AddProduct
 {
-    public Guid? Id { get; set; }
-
+    [Display(Name = "Product Name")]
     [Required(ErrorMessage = "Il nome è obbligatorio.")]
     [StringLength(100, ErrorMessage = "Il nome non può superare i 100 caratteri.")]
-    public string Name { get; set; } = string.Empty;
+    public string? Name { get; set; }
 
+    [Display(Name = "Product description")]
     [StringLength(500, ErrorMessage = "La descrizione non può superare i 500 caratteri.")]
     public string? Description { get; set; }
 
+    [Display(Name = "Product category")]
     [Required(ErrorMessage = "Selezionare una categoria.")]
     public Guid CategoryId { get; set; }
 
+    [Display(Name = "Product price")]
     [Required(ErrorMessage = "Il prezzo è obbligatorio.")]
     [Range(0.01, 100000, ErrorMessage = "Il prezzo deve essere compreso tra 0.01 e 100000.")]
     public decimal Price { get; set; }
-
+    
     public List<Category>? Categories { get; set; } // Per popolare la select
 }
--- Ecommerce/obj/Debug/net9.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/obj/Debug/net9.0/Ecommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70fc5862469511a77026ffc102e62f6770f73b0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24232b4413a3c09262e1de34301d711ccb70ba9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
